cefb1ba3f0e09543f523b3bc4be3525a1ad256b8
Kate Sorotos <katesorotos@Kates-MacBook-Air.local>
2020-04-20 17:53:50 +0100

    Lists & Conditionals

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component {
   state = {
+    userInput: '',
     username: 'Martini Mama'
   }
 
@@ -14,7 +17,25 @@ class App extends Component {
     this.setState({username: event.target.value});
   }
 
-  render() {
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value});
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText=text.join('');
+    this.setState({userInput : updatedText});
+    }
+
+    render () {
+      const charList = this.state.userInput.split('').map((ch, index) => {
+        return <Char 
+          character={ch} 
+          key={index}
+          clicked={() => this.deleteCharHandler(index)} />;
+      });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,7 +51,14 @@ class App extends Component {
       <UserOutput userName={this.state.username} />
       {/* <UserOutput userName={this.state.username} />
       <UserOutput userName='Kate'/> */}
+
+      <hr/>
+      <input type="text" onChange={this.inputChangedHandler} value={this.state.userInput}/>
+      <p>{this.state.userInput}</p>
+      <Validation inputLength={this.state.userInput.length}/>
+      {charList}
       </div>
+      
     );
   }
 }
